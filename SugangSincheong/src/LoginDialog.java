1ad23ea9ad251f360fb014257e154c78e0805fa1
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2022-10-24 13:25:52 +0900

    [TEST] test

---
+++ SugangSincheong/src/LoginDialog.java
@@ -0,0 +1,67 @@
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class LoginDialog extends JDialog {
+	private static final long serialVersionUID = 1L;
+
+	private JTextField tfId;
+	private JPasswordField tfPassword;
+	
+	private SLogin sLogin;
+	
+	public LoginDialog(Frame parent) {
+		super(parent);
+		
+		LayoutManager layoutManager = new GridLayout(3, 2);
+		this.setLayout(layoutManager);
+				
+		JLabel lbId = new JLabel("아이디");
+		this.add(lbId);
+		
+		this.tfId = new JTextField();
+		this.tfId.setColumns(10);
+		this.add(this.tfId);
+		
+		JLabel lbPassword = new JLabel("비밀번호");
+		this.add(lbPassword);
+		
+		this.tfPassword = new JPasswordField();
+		this.tfPassword.setColumns(10);
+		this.add(this.tfPassword);
+		
+		JButton btLogin = new JButton("로그인");
+		this.add(btLogin);
+		
+		ActionHandler actionHandler = new ActionHandler();
+		btLogin.addActionListener(actionHandler);
+		
+		this.sLogin = new SLogin();
+	}
+	
+	private void login() {
+		String id = this.tfId.getText();
+		String password = this.tfPassword.getText();
+		
+		VLogin vLogin = sLogin.login(id);
+		if (vLogin == null) {
+			// 아이디가 없거나 비밀번호가 틀렸습니다.
+		}
+
+	}
+	private class ActionHandler implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			login();
+		}		
+	}
+}
